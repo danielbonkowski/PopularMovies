7e2867a7435ed63a6e6f02efd26e87da161aa14a
Daniel Bonkowski <nicosnakecod@gmail.com>
2020-05-26 15:57:04 +0200

    Implements Parcelable in Movie class

--- app/src/main/java/android/example/com/popularmovies/data/Movie.java
+++ app/src/main/java/android/example/com/popularmovies/data/Movie.java
@@ -1,11 +1,15 @@
 package android.example.com.popularmovies.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 import java.util.Date;
 
-public class Movie implements Serializable {
+public class Movie implements Parcelable {
 
     private int movieId;
     private String originalTitle;
@@ -24,6 +28,31 @@ public class Movie implements Serializable {
         this.releaseDate = releaseDate;
     }
 
+
+    private Movie(Parcel in) {
+        movieId = in.readInt();
+        originalTitle = in.readString();
+        moviePoster = in.readString();
+        plotSynopsis = in.readString();
+        if (in.readByte() == 0) {
+            userAverageRating = null;
+        } else {
+            userAverageRating = in.readDouble();
+        }
+        releaseDate = new Date(in.readLong());
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getOriginalTitle() {
         return originalTitle;
@@ -71,5 +100,32 @@ public class Movie implements Serializable {
 
     public void setMovieId(int movieId) {
         this.movieId = movieId;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(movieId);
+        dest.writeString(originalTitle);
+        dest.writeString(moviePoster);
+        dest.writeString(plotSynopsis);
+        dest.writeDouble(userAverageRating);
+        dest.writeLong(releaseDate.getTime());
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Id: " + movieId +
+                "\nOriginal title: " + originalTitle +
+                "\nMovie poster: "  + moviePoster +
+                "\nPlot synopsis: " + plotSynopsis +
+                "\nUser average rating: " + userAverageRating +
+                "\nRelease date: " + releaseDate;
     }
 }
